44ef1d0864602d9f233c66dc182d6e8e84d41713
VickriFahrurozi <VickriFahrurozi@gmail.com>
2022-09-13 00:18:24 +0700

    feat : fix bug

--- helper/db_connections.js
+++ helper/db_connections.js
@@ -1,6 +1,6 @@
 /** @format */
 
-const mysql = require('mysq2');
+const mysql = require('mysql2');
 const { host, user, password, database } = process.env;
 const db = mysql.createPool({
 	connectionLimit: 100,
